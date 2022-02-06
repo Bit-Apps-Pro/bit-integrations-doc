1c9fa3eefda3216aae5e5f032aff889a69b81479
rakibul hasan <96240659+wayesqurani@users.noreply.github.com>
2022-02-06 11:46:11 +0600

    Welcome & CF7

--- sidebars.js
+++ sidebars.js
@@ -1,14 +1,23 @@
 module.exports = {
   someSidebar: [
     {
-      "Getting started": [
-        "Getting Started/welcome",
+      "Getting started": ["Getting Started/welcome"],
+    },
+    {
+      "Contact Form 7": [
+        "Contact Form 7/activecampaign",
+        "Contact Form 7/encharge",
+        "Contact Form 7/rapidmail",
+        "Contact Form 7/mailchimp",
+        "Contact Form 7/telegram",
+        "Contact Form 7/zohocrm",
+        "Contact Form 7/zohobigin",
+        "Contact Form 7/zohorecruit",
+        "Contact Form 7/zohocampaign",
+        "Contact Form 7/zohomarketinghub",
       ],
     },
-    "Response Management/response-management",
+    // "Response Management/response-management",
   ],
-  changelog: [
-    "changelog/overview",
-    "changelog/prooverview",
-  ],
+  changelog: ["changelog/overview", "changelog/prooverview"],
 };

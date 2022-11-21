4c0fd5b77f62acad6d2960e70dd4b30bfb39adeb
Al <almizan1040@gmail.com>
2022-11-21 15:59:29 +0600

    Mailercloud added

--- sidebars.js
+++ sidebars.js
@@ -81,6 +81,7 @@ module.exports = {
         "actions/klaviyo",
         "actions/learndash",
         "actions/mailchimp",
+        "actions/mailercloud",
         "actions/mailerlite",
         "actions/mailpoet",
         "actions/mautic",
